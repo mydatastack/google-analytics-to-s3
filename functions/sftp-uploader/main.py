5f6c24436f2a8192d13639b6bb27d20c1497ea47
tarasowski <dimitri+github@tarasowski.de>
2019-09-18 18:28:44 +0200

    working sftp upload

--- functions/sftp-uploader/main.py
+++ functions/sftp-uploader/main.py
@@ -5,8 +5,19 @@ from datetime import date, datetime, timedelta
 import boto3
 import re
 import pysftp
+from os.path import expanduser
+import io
+from functools import reduce, partial
+
+pipe = lambda fns: lambda x: reduce(lambda v, f: f(v), fns, x)
+
 s3 = boto3.resource("s3")
 s3_client = boto3.client("s3")
+ssm = boto3.client("ssm")
+
+key_password = ssm.get_parameter(Name="odoscope_key_password")["Parameter"]["Value"]
+key_username = ssm.get_parameter(Name="odoscope_username")["Parameter"]["Value"]
+key_hostname = ssm.get_parameter(Name="odoscope_host")["Parameter"]["Value"]
 
 yesterday = (datetime.now() - timedelta(1))
 yesterday_year = yesterday.strftime("%Y")
@@ -22,49 +33,76 @@ def get_bucket_url():
         return "tarasowski-main-dev-machine-googleanal-databucket-cl4te8jo5be1"
 
 S3_BUCKET = get_bucket_url()
-key = "/aggregated/ga" + "/year=" + yesterday_year + "/month=" + yesterday_month + "/day=" + yesterday_day + "/"
 date_folder = "/year=" + yesterday_year + "/month=" + yesterday_month + "/day=" + yesterday_day + "/"
-sessions = key + "sessions/"
-products = key + "products/"
-pageviews = key + "pageviews/"
-events = key + "events/"
-
-def filter_files(keys: list) -> list:
-    return [ file_object for file_object in keys if bool(re.findall(r"^aggregated\/ga" + re.escape(date_folder) + "(sessions|pageviews|products|events)" + ".*csv$", file_object)) ] 
 
 def list_bucket_content(bucket):
     return [objects.key for objects in s3.Bucket(bucket).objects.all()]
 
+def filter_files(keys: list) -> list:
+    return [ file_object for file_object in keys if bool(re.findall(r"^aggregated\/ga" + re.escape(date_folder) + "(sessions|pageviews|products|events)" + ".*csv$", file_object)) ] 
+
 def tmp_download(keys: list) -> ():
     return [s3_client.download_file(S3_BUCKET, key, "/tmp/" + key.split("/")[5] + "_" + file_date + ".csv") for key in keys]
 
-def zip_tmp():
-    shutil.make_archive("/tmp/" + yesterday_year + yesterday_month + yesterday_day, "zip", "/tmp")
+def zip_tmp(y_year, y_month, y_day, *args) -> ():
+    shutil.make_archive("/tmp/" + y_year + y_month + y_day, "zip", "/tmp")
 
-def ftp_upload():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    port = 22
-    transport = paramiko.Transport((host, port))
-    ssh.connect(host, port=port, look_for_keys=False, pkey = mykey) 
-    
+def get_ssh_key(key_password: str, *args):
+    try:
+        f = open(os.path.join(expanduser('~'), ".ssh", "odoscope-new.key"), "r")
+        s = f.read()
+        f.close()
+        keyfile = io.StringIO(s)
+        mykey = paramiko.RSAKey.from_private_key(keyfile, password= key_password)
+    except Exception as e:
+        print(e)
+        raise Exception("couldn't get the ssh key") 
+    else:
+        return mykey
 
+def sftp_connect(mykey: str):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname = key_hostname, username = key_username, pkey= mykey)
+    except Exception as e:
+        print(e)
+        raise Exception("cloudn't connect to the sftp")
+    else:
+        return ssh
+
+def sftp_upload(ssh) -> ():
+    try:
+        ftp_client = ssh.open_sftp()
+        ftp_client.put(f"/tmp/{file_date}.zip", f"/incoming/{file_date}.zip")
+        ftp_client.close()
+    except Exception as e:
+        print(e)
+        raise Exception("count't upload the files to sftp")
+    else:
+        return "success"
 
 def handler(event: dict, ctx: dict) -> ():
-    #x = list_bucket_content(S3_BUCKET)
-    #y = filter_files(x)
-    #z = tmp_download(y)
-    #i = zip_tmp()
-    ftp_upload()
+    return pipe([
+        list_bucket_content,
+        filter_files,
+        tmp_download,
+        partial(zip_tmp, yesterday_year, yesterday_month, yesterday_day),
+        partial(get_ssh_key, key_password),
+        sftp_connect,
+        sftp_upload,
+    ]) (S3_BUCKET)
 
 if __name__ == '__main__':
     import unittest
 
     class TestHandler(unittest.TestCase):
-        def test_read_file_data(self):
-            pass
         def test_run_handler(self):
             self.assertEqual(handler(None, None), "success")
+
+        def test_filter_files(self):
+            url = f"aggregated/ga{date_folder}events/part-00000-3418d3cf-68e9-486f-ae13-f60d3d44ed94-c000.csv"
+            self.assertEqual(filter_files([url]), [url])
 
     unittest.main()
 
